920ba1ef91ee5cc696836ea73290948612f5876b
adiomas <adrijan.omicevic@q.agency>
2024-01-23 21:21:16 +0100

    changes

--- script.js
+++ script.js
@@ -81,8 +81,95 @@ function generateSignature() {
   //       </table>
   //   </div>
   // `;
+  signaturePreview.innerHTML = `<div>
+    <table cellpadding="0" width="300" style="border-collapse: collapse; font-size: 13.4px;">
+        <tbody>
+            <tr>
+                <td style="margin: 0.1px; padding: 0px;">
+                    <table cellpadding="0" style="border-collapse: collapse; text-align: left;">
+                        <tbody>
+                            <tr>
+                                <td valign="top" style="margin: 0.1px; padding: 0px 12px 0px 0px;">
+                                    <a href="${website}" target="_blank">
+                                        <img src="${imageURL}" width="100" alt="Euroyacht" style="display: block; min-width: 100px;">
+                                    </a>
+                                </td>
+                                <td valign="top" style="border-left-width: 1px; border-left-style: solid; border-left-color: rgb(35, 71, 149); margin: 0.1px; padding: 0px 0px 0px 12px; font-family: Arial, Helvetica, sans-serif;">
+                                    <table cellpadding="0" style="border-collapse: collapse;">
+                                        <tbody>
+                                            <tr>
+                                                <td style="margin: 0.1px; padding: 0px 0px 8px; font-weight: 600; font-size: 17.5px; color: rgb(35, 71, 149);">
+                                                    ${fullName}
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td style="margin: 0.1px; padding: 0px 0px 8px; font-size: 13.4px; color: rgb(0, 0, 1);">
+                                                    <span>${jobRole}</span>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td style="margin: 0.1px; padding: 0px; font-size: 13.4px; color: rgb(0, 0, 1);">
+                                                    <span style="color: rgb(35, 71, 149); font-weight: 600;">phone:&nbsp;</span>
+                                                    <a href="tel://${mobile}" target="_blank" style="color: rgb(0, 0, 1); text-decoration: none;">
+                                                        ${mobile}
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td style="margin: 0.1px; padding: 0px; font-size: 13.4px; color: rgb(0, 0, 1);">
+                                                    <span style="color: rgb(35, 71, 149); font-weight: 600;">email:&nbsp;</span>
+                                                    <a href="mailto:${email}" target="_blank" style="color: rgb(0, 0, 1); text-decoration: none;">
+                                                        ${email}
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td style="margin: 0.1px; padding: 0px; font-size: 13.4px; color: rgb(0, 0, 1);">
+                                                    <span style="color: rgb(35, 71, 149); font-weight: 600;">website: &nbsp;</span>
+                                                    <a href="${website}" target="_blank" style="color: rgb(0, 0, 1); text-decoration: none;">www.euroyacht.hr
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td style="margin: 0.1px; padding: 12px 0px 0px; font-size: 13.4px; color: rgb(0, 0, 1);">
+                                                    <table cellpadding="0" style="border-collapse: collapse;">
+                                                        <tbody>
+                                                            <tr>
+                                                                <td style="margin: 0.1px; padding: 0px 5px 0px 0px;">
+                                                                    <a href="https://instagram.com/euroyacht" target="_blank">
+                                                                        <img width="22" src="https://img.mysignature.io/s/1/f/a/1faf0ad2-8164-56d0-9b65-6d62a56cd22c.png?uid=654367" alt="Instagram" style="display: block;">
+                                                                    </a>
+                                                                </td>
+                                                                <td style="margin: 0.1px; padding: 0px 5px 0px 0px;">
+                                                                    <a href="https://facebook.com/euroyacht.hr" target="_blank">
+                                                                        <img width="22" src="https://img.mysignature.io/s/d/9/a/d9a505f7-c884-5a57-bb57-0542959d3c27.png?uid=654367" alt="Facebook" style="display: block;">
+                                                                    </a>
+                                                                </td>
+                                                                <td style="margin: 0.1px; padding: 0px 5px 0px 0px;">
+                                                                    <a href="https://tiktok.com/@euroyachtcharter" target="_blank">
+                                                                        <img width="22" src="https://img.mysignature.io/s/6/d/e/6de544ab-6792-54e1-8678-ea1ecdc699b6.png?uid=654367" alt="TikTok" style="display: block;">
+                                                                    </a>
+                                                                </td>
+                                                            </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+  </div>
+    `;
+
   //   signaturePreview.innerHTML = `<div>
-  //   <table cellpadding="0" width="300" style="border-collapse: collapse; font-size: 13.4px;">
+  //   <table cellpadding="0" width="300" style="border-collapse: collapse; font-size: 10px;">
   //       <tbody>
   //           <tr>
   //               <td style="margin: 0.1px; padding: 0px;">
@@ -91,24 +178,24 @@ function generateSignature() {
   //                           <tr>
   //                               <td valign="top" style="margin: 0.1px; padding: 0px 12px 0px 0px;">
   //                                   <a href="${website}" target="_blank">
-  //                                       <img src="${imageURL}" width="100" alt="Euroyacht" style="display: block; min-width: 100px;">
+  //                                       <img src="${imageURL}" width="80" alt="Euroyacht" style="display: block; min-width: 80px;">
   //                                   </a>
   //                               </td>
   //                               <td valign="top" style="border-left-width: 1px; border-left-style: solid; border-left-color: rgb(35, 71, 149); margin: 0.1px; padding: 0px 0px 0px 12px; font-family: Arial, Helvetica, sans-serif;">
   //                                   <table cellpadding="0" style="border-collapse: collapse;">
   //                                       <tbody>
   //                                           <tr>
-  //                                               <td style="margin: 0.1px; padding: 0px 0px 8px; font-weight: 600; font-size: 17.5px; color: rgb(35, 71, 149);">
+  //                                               <td style="margin: 0.1px; padding: 0px 0px 8px; font-weight: 600; font-size: 13px; color: rgb(35, 71, 149);">
   //                                                   ${fullName}
   //                                               </td>
   //                                           </tr>
   //                                           <tr>
-  //                                               <td style="margin: 0.1px; padding: 0px 0px 8px; font-size: 13.4px; color: rgb(0, 0, 1);">
+  //                                               <td style="margin: 0.1px; padding: 0px 0px 8px; font-size: 10px; color: rgb(0, 0, 1);">
   //                                                   <span>${jobRole}</span>
   //                                               </td>
   //                                           </tr>
   //                                           <tr>
-  //                                               <td style="margin: 0.1px; padding: 0px; font-size: 13.4px; color: rgb(0, 0, 1);">
+  //                                               <td style="margin: 0.1px; padding: 0px; font-size: 10px; color: rgb(0, 0, 1);">
   //                                                   <span style="color: rgb(35, 71, 149); font-weight: 600;">phone:&nbsp;</span>
   //                                                   <a href="tel://${mobile}" target="_blank" style="color: rgb(0, 0, 1); text-decoration: none;">
   //                                                       ${mobile}
@@ -116,7 +203,7 @@ function generateSignature() {
   //                                               </td>
   //                                           </tr>
   //                                           <tr>
-  //                                               <td style="margin: 0.1px; padding: 0px; font-size: 13.4px; color: rgb(0, 0, 1);">
+  //                                               <td style="margin: 0.1px; padding: 0px; font-size: 10px; color: rgb(0, 0, 1);">
   //                                                   <span style="color: rgb(35, 71, 149); font-weight: 600;">email:&nbsp;</span>
   //                                                   <a href="mailto:${email}" target="_blank" style="color: rgb(0, 0, 1); text-decoration: none;">
   //                                                       ${email}
@@ -124,14 +211,14 @@ function generateSignature() {
   //                                               </td>
   //                                           </tr>
   //                                           <tr>
-  //                                               <td style="margin: 0.1px; padding: 0px; font-size: 13.4px; color: rgb(0, 0, 1);">
+  //                                               <td style="margin: 0.1px; padding: 0px; font-size: 10px; color: rgb(0, 0, 1);">
   //                                                   <span style="color: rgb(35, 71, 149); font-weight: 600;">website: &nbsp;</span>
   //                                                   <a href="${website}" target="_blank" style="color: rgb(0, 0, 1); text-decoration: none;">www.euroyacht.hr
   //                                                   </a>
   //                                               </td>
   //                                           </tr>
   //                                           <tr>
-  //                                               <td style="margin: 0.1px; padding: 12px 0px 0px; font-size: 13.4px; color: rgb(0, 0, 1);">
+  //                                               <td style="margin: 0.1px; padding: 12px 0px 0px; font-size: 10px; color: rgb(0, 0, 1);">
   //                                                   <table cellpadding="0" style="border-collapse: collapse;">
   //                                                       <tbody>
   //                                                           <tr>
@@ -167,93 +254,6 @@ function generateSignature() {
   //   </table>
   // </div>
   //   `;
-
-  signaturePreview.innerHTML = `<div>
-  <table cellpadding="0" width="300" style="border-collapse: collapse; font-size: 10px;">
-      <tbody>
-          <tr>
-              <td style="margin: 0.1px; padding: 0px;">
-                  <table cellpadding="0" style="border-collapse: collapse; text-align: left;">
-                      <tbody>
-                          <tr>
-                              <td valign="top" style="margin: 0.1px; padding: 0px 12px 0px 0px;">
-                                  <a href="${website}" target="_blank">
-                                      <img src="${imageURL}" width="80" alt="Euroyacht" style="display: block; min-width: 80px;">
-                                  </a>
-                              </td>
-                              <td valign="top" style="border-left-width: 1px; border-left-style: solid; border-left-color: rgb(35, 71, 149); margin: 0.1px; padding: 0px 0px 0px 12px; font-family: Arial, Helvetica, sans-serif;">
-                                  <table cellpadding="0" style="border-collapse: collapse;">
-                                      <tbody>
-                                          <tr>
-                                              <td style="margin: 0.1px; padding: 0px 0px 8px; font-weight: 600; font-size: 13px; color: rgb(35, 71, 149);">
-                                                  ${fullName}
-                                              </td>
-                                          </tr>
-                                          <tr>
-                                              <td style="margin: 0.1px; padding: 0px 0px 8px; font-size: 10px; color: rgb(0, 0, 1);">
-                                                  <span>${jobRole}</span>
-                                              </td>
-                                          </tr>
-                                          <tr>
-                                              <td style="margin: 0.1px; padding: 0px; font-size: 10px; color: rgb(0, 0, 1);">
-                                                  <span style="color: rgb(35, 71, 149); font-weight: 600;">phone:&nbsp;</span>
-                                                  <a href="tel://${mobile}" target="_blank" style="color: rgb(0, 0, 1); text-decoration: none;">
-                                                      ${mobile}
-                                                  </a>
-                                              </td>
-                                          </tr>
-                                          <tr>
-                                              <td style="margin: 0.1px; padding: 0px; font-size: 10px; color: rgb(0, 0, 1);">
-                                                  <span style="color: rgb(35, 71, 149); font-weight: 600;">email:&nbsp;</span>
-                                                  <a href="mailto:${email}" target="_blank" style="color: rgb(0, 0, 1); text-decoration: none;">
-                                                      ${email}
-                                                  </a>
-                                              </td>
-                                          </tr>
-                                          <tr>
-                                              <td style="margin: 0.1px; padding: 0px; font-size: 10px; color: rgb(0, 0, 1);">
-                                                  <span style="color: rgb(35, 71, 149); font-weight: 600;">website: &nbsp;</span>
-                                                  <a href="${website}" target="_blank" style="color: rgb(0, 0, 1); text-decoration: none;">www.euroyacht.hr
-                                                  </a>
-                                              </td>
-                                          </tr>
-                                          <tr>
-                                              <td style="margin: 0.1px; padding: 12px 0px 0px; font-size: 10px; color: rgb(0, 0, 1);">
-                                                  <table cellpadding="0" style="border-collapse: collapse;">
-                                                      <tbody>
-                                                          <tr>
-                                                              <td style="margin: 0.1px; padding: 0px 5px 0px 0px;">
-                                                                  <a href="https://instagram.com/euroyacht" target="_blank">
-                                                                      <img width="22" src="https://img.mysignature.io/s/1/f/a/1faf0ad2-8164-56d0-9b65-6d62a56cd22c.png?uid=654367" alt="Instagram" style="display: block;">
-                                                                  </a>
-                                                              </td>
-                                                              <td style="margin: 0.1px; padding: 0px 5px 0px 0px;">
-                                                                  <a href="https://facebook.com/euroyacht.hr" target="_blank">
-                                                                      <img width="22" src="https://img.mysignature.io/s/d/9/a/d9a505f7-c884-5a57-bb57-0542959d3c27.png?uid=654367" alt="Facebook" style="display: block;">
-                                                                  </a>
-                                                              </td>
-                                                              <td style="margin: 0.1px; padding: 0px 5px 0px 0px;">
-                                                                  <a href="https://tiktok.com/@euroyachtcharter" target="_blank">
-                                                                      <img width="22" src="https://img.mysignature.io/s/6/d/e/6de544ab-6792-54e1-8678-ea1ecdc699b6.png?uid=654367" alt="TikTok" style="display: block;">
-                                                                  </a>
-                                                              </td>
-                                                          </tr>
-                                                      </tbody>
-                                                  </table>
-                                              </td>
-                                          </tr>
-                                      </tbody>
-                                  </table>
-                              </td>
-                          </tr>
-                      </tbody>
-                  </table>
-              </td>
-          </tr>
-      </tbody>
-  </table>
-</div>
-  `;
 
 
   // Show copy button
